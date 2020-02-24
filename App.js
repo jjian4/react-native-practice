bb1f3941ab63588829eda421ad31a4f489280d39
jjian4 <jjian@umich.edu>
2020-02-23 21:28:02 -0800

    Header component

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Header title="Shopping List" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
 
 export default App;
